91ae3316e077b4ea3756264a16b210f1fc4ba854
Tina Krutzenbichler <christina.krutzenbichler@insa-lyon.fr>
2017-03-12 22:27:15 +0100

    Tried some more files and the input command, need to put this in a seperate file tomorrow

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 // my first program in C++
 #include <iostream>
+#include "parser.hpp"
+
 
 int main()
 {
   std::cout << "Hello World!";
   std::cout << "Hello Tina!";
+  int i;
+  std::cout << "Please enter an integer value: ";
+  std::cin >> i;
+  std::cout << "The value you entered is " << i;
+  std::cout << " and its double is " << i*2 << ".\n";
+  return 0;
 
 }
